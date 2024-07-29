6dd8638a01865bf8d8d9bb151075879115380ad1
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-07-29 21:53:59 +0300

    commit

--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -1,5 +1,4 @@
 // @flow
-import * as React from 'react';
 import {Note} from "../../App";
 import styles from "./notes.module.css"
 import {NoteWidget} from "../NoteWidget/NoteWidget";
@@ -12,7 +11,7 @@ export const Notes = (props: ContentProps) => {
     const {notes, removeNote} = props;
     return (
         <div className={styles["container"]}>
-            {notes.map(note => <NoteWidget note={note} removeNote={removeNote}></NoteWidget>)}
+            {notes.map(note => <NoteWidget key={note.id} note={note} removeNote={removeNote}></NoteWidget>)}
         </div>
     );
 };
